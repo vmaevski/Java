abb57766a03322a49683c713fe6c125896fd9033
vmaevski <vmaevski@mail.ru>
2022-09-10 01:31:13 +0700

    Task Ways cmpleted!!!

--- HW/HW_S01_Ways.java
+++ HW/HW_S01_Ways.java
@@ -9,31 +9,35 @@
 // ответ: нет решения.
 // *Подумать над тем, как сделать минимальное количество команд
 
-// В решении находится кол-во путей
 
 // import javax.swing.text.html.HTMLDocument.HTMLReader.ParagraphAction;
 
 public class HW_S01_Ways {
     public static void main(String[] args) {
-        System.out.println(solve(2, 30, 2, 4));
-        System.out.println(recursion_solve(2, 30, 2, 4));
-        
+        int start = 2; int end = 18; int com1 = 5; int com2 = 2;
+        System.out.println(how_many_ways(start, end, com1, com2));
+        System.out.println(recursion_solve(start, end, com1, com2));
+        one_way(start, end, com1, com2);
     }
-
-    static int solve(int start, int end, int com1, int com2) {
+// В решении 1) выводится в терминал массив с индексами; 2) находится кол-во путей:
+    static int how_many_ways(int start, int end, int com1, int com2) {
         int[] ways = new int[end + 1];
         ways[start] = 1;
+        int start_index = start + com1  < start * com2 ? start + com1 : start * com2;
+        System.out.format("(%d)%d ", start, ways[start]);
 
-        for (int index = start + com1; index <= end; index++) {
+        for (int index = start_index; index <= end; index++) {
             if (index % com2 == 0) {
-                ways[index] = ways[index - com1] + ways[index / com2];
+                ways[index] = (index - com1 < 0 ? 0 : ways[index - com1]) + ways[index / com2];
             } else {
-                ways[index] = ways[index - com1];
+                ways[index] = index - com1 < 0 ? 0 : ways[index - com1];
             }
+            System.out.format("(%d)%d ", index, ways[index]);
         }
+        System.out.format("\n");
         return ways[end];
     }    
-
+// В решении находится кол-во путей (при помощи рекурсии):
     static int recursion_solve(int start, int end, int com1, int com2) {
         if (end == start) return 1;
         else if (end < start) return 0;
@@ -43,5 +47,52 @@ public class HW_S01_Ways {
             else return recursion_solve(start, end - com1, com1, com2);
         }
     }
-    
+
+// Метод возвращает массив
+    static int[] arr_how_many_ways(int start, int end, int com1, int com2) {
+        int[] ways = new int[end + 1];
+        ways[start] = 1;
+        int start_index = start + com1  < start * com2 ? start + com1 : start * com2;
+        for (int index = start_index; index <= end; index++) {
+            if (index % com2 == 0) {
+                ways[index] = (index - com1 < 0 ? 0 : ways[index - com1]) + ways[index / com2];
+                
+            } else {
+                ways[index] = index - com1 < 0 ? 0 : ways[index - com1];
+            }
+        }
+        return ways;
+    }     
+
+// Метод возвращает один путь    
+    static void one_way(int start, int end, int com1, int com2){
+        int[] ways = arr_how_many_ways(start, end, com1, com2);
+        if (ways[end] != 0){
+            // System.out.format("Пути есть!");   
+            String res = " = " + end;
+            int value = end;
+            while (value >= start) {
+                if (value == start) {
+                    res = start + res;
+                    break;
+                }
+                if ((value - com1 >= start) && (ways[value - com1] != 0)){
+                    value -= com1;
+                    res = " + " + com1 + res;
+                } else{
+                    if ((value % com2 == 0) & (ways[value / 2] !=0)) {
+                        value /= com2;
+                        res = " x " + com2 + res;
+                    }
+                }
+            }
+            System.out.println(res);
+        } else{
+            System.out.format("Нет путей от %d до %d", start, end);    
+        }
+        
+    }
+
 }
+
+
